f744861449f732dc0f5ae0eaffaf89a140b2adac
moritzrp <50995474+moritzrp@users.noreply.github.com>
2024-02-22 16:50:05 +0100

    add lsps

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,8 @@ return {
 				ensure_installed = {
 					"lua_ls",
 					"pyright",
+                    "marksman",
+                    "ansiblels"
 				},
 			})
 		end,
@@ -19,9 +21,14 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+			lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
 			lspconfig.pyright.setup({})
+			lspconfig.marksman.setup({})
+            lspconfig.ansiblels.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
