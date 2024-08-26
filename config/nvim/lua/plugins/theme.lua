92115c8661273bfd0c2113d9a3f1f5aab8fa434c
moritzrp <50995474+moritzrp@users.noreply.github.com>
2024-08-26 13:46:32 +0200

    add theme options

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -6,6 +6,12 @@ return {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     "rebelot/kanagawa.nvim",
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      background = {
+        dark = "wave",
+        light = "lotus",
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
