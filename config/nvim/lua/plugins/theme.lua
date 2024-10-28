025d6a60fd94225e1fd909cc556afeb5170e45d4
moritzrp <50995474+moritzrp@users.noreply.github.com>
2024-10-28 19:22:43 +0100

    change theme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,18 +1,11 @@
 return {
   {
     "rebelot/kanagawa.nvim",
-    priority = 1000,
-    opts = {
-      background = {
-        dark = "wave",
-        light = "lotus",
-      },
-    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "tokyonight-night",
     },
   },
 }
