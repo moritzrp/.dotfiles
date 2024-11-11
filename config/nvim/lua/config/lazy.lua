dfee0d67bef4980d0190e9544b8630d526eba16d
moritzrp <50995474+moritzrp@users.noreply.github.com>
2024-11-11 15:11:44 +0100

    add mini comment

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "plugins" },
   },
