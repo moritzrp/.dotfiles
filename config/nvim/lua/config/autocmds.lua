960f6657362739e1b533b11eb99b7d6b83319c66
moritzrp <50995474+moritzrp@users.noreply.github.com>
2025-05-16 10:37:42 +0200

    add detection for ansible

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = {
+    "*/playbooks/*.yml",
+    "*/playbooks/roles/*/*/*.yml",
+    "*/roles/*/*/*.yml",
+    "*/tasks/*.yml",
+    "*/defaults/*.yml",
+    "*/host_vars/*.yml",
+  },
+  callback = function()
+    vim.bo.filetype = "yaml.ansible"
+  end,
+})
