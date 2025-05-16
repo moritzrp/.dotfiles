f788c5f795a70db7b58b57bec8e7e56e913c2112
moritzrp <50995474+moritzrp@users.noreply.github.com>
2025-05-16 10:37:51 +0200

    change theme to onedark

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,17 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+      require("onedark").load()
+    end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight-night",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "tokyonight-night",
+  --   },
+  -- },
 }
