b543ec81f9652aa89b2ac4c6d480e9743b542933
moritzrp <50995474+moritzrp@users.noreply.github.com>
2025-07-27 13:50:31 +0200

    update polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,9 +3,9 @@
 sleep .5
 
 if ! pgrep -x polybar; then
-	polybar base &
+  polybar example -c ~/.config/polybar/config.ini &
 else
-	pkill -USR1 polybar
+  pkill -USR1 polybar
 fi
 
 echo "Bars launched..."
